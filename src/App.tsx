40d727b5e1bee481f9f204a9e31cecf651c74ce5
Casey C <casey_c0808@outlook.com>
2022-10-20 21:04:25 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "./App.css";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import Header from './components/Header';
+import MenuBar from "./components/MenuBar";
+
+const theme = createTheme({
+  typography: {
+    fontSize: 14,
+    fontFamily: 'Nunito Sans'
+  }
+})
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <MenuBar />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography component="div" sx={{ flexGrow: 1, fontWeight: 800, fontSize: 24 }}>
+            Where in the world?
+          </Typography>
+          <Button color="inherit">Dark Mode</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header
